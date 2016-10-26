f262e300535559fbafb10ec6d975b34a9449d3b6
puka89 <ludia.kud@abv.bg>
2016-10-26 23:41:59 +0300

    Update 02_lol.c

--- B/03/14/02_lol.c
+++ B/03/14/02_lol.c
@@ -1,31 +1,25 @@
-
-
-
-
 #include "stdio.h"
-
 
 int main() {
 
-  int n, s,cif=0;
-  float avg=0;
-scanf("%d",&n);
+  int n,s,q,i;
+  float m;
+  q=0;
+  m=0;
+  
+  scanf("%d",&n);
+  for(i=0;0<n;++i)
+  {
+  s = n%10;
+  q=q+s;
+  m=(float)q/(i+1);
+  n /=10;
+  }
+  if(m>7){
+  printf("heavy");
+  }else{
+  printf("light");
+  }
 
-for (int i = 0; 0 < n; ++i)
-{
-s = n%10;
-cif = cif + s;
-avg = (float)cif/(i+1);
-n /=10;
-}
-if(avg > 7){
-
-printf("heavy");
-
-}else{
-
-printf("light");
-}
-
-return 0;
+  return 0;
 }
